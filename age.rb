f726880b47fab2a48a798388dff917da2273374f
Sam Peng <sampeng618@gmail.com>
2025-02-25 21:00:40 +0800

    Add initial ruby exercises

---
+++ age.rb
@@ -0,0 +1,12 @@
+puts "How old are you?"
+
+age = gets.chomp.to_i
+ten_years = age + 10
+twenty_years = age + 20
+thirty_years = age + 30
+forty_years = age + 40
+
+puts "In 10 years you will be #{ten_years} years old!"
+puts "In 20 years you will be #{twenty_years} years old!"
+puts "In 30 years you will be #{thirty_years} years old!"
+puts "In 40 years you will be #{forty_years} years old!"
