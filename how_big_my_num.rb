d39201c7080821b428c14f3245adffdafcc8c6d7
Sam Peng <sampeng618@gmail.com>
2025-02-26 15:57:14 +0800

    finished how_big_my_num.rb

--- how_big_my_num.rb
+++ how_big_my_num.rb
@@ -1,12 +1,26 @@
-print "Please enter a number between 0 - 100 :"
-num = gets.chomp.to_i
+letters = ("a".."z")
+cap_letters = ("A".."Z")
+
+print "Please enter a number between 0 - 100 : "
+num = gets.chomp
+
+if num.chars.all? {|char| letters.include? char }
+  puts "You can't enter letters, please enter a number between 0 - 100!"
+  exit
+elsif
+  num.chars.all? {|char| cap_letters.include? char }
+  puts "You can't enter letters, please enter a number between 0 - 100!"
+  exit
+else
+  num = num.to_i
+end
 
 if (0 <= num) && (num <= 50)
-  puts "Your number is between 0 and 50!"
+  puts "#{num} is between 0 and 50!"
 elsif (51 <= num) && (num <= 100)
-  puts "Your number is between 51 and 100!"
+  puts "#{num} is between 51 and 100!"
 elsif num > 100
-  puts "Your number is greater than 100, please enter a number between 0 - 100!"
+  puts "#{num} is greater than 100, please enter a number between 0 - 100!"
 else
-  puts "Invalid! Please enter a number between 0 - 100!"
+  puts "You can't enter a negative number! Please enter a number between 0 - 100!"
 end
